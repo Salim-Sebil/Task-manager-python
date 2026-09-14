2776b25884e695159cc486042e18a77cc1edaddd
salim-sebil <salim.sebil@student.moringaschool.com>
2026-09-15 00:46:40 +0300

    final changes

--- task_manager/validation.py
+++ task_manager/validation.py
@@ -2,19 +2,25 @@ from datetime import datetime
 
 
 def validate_task_title(title):
-    if not isinstance(title, str) or len(title.strip()) == 0:
+    if not isinstance(title, str):
+        raise ValueError("Task title must be a string.")
+    if len(title.strip()) == 0:
         raise ValueError("Task title cannot be empty.")
     return title.strip()
 
 
 def validate_task_description(description):
-    if not isinstance(description, str) or len(description.strip()) == 0:
+    if not isinstance(description, str):
+        raise ValueError("Task description must be a string.")
+    if len(description.strip()) == 0:
         raise ValueError("Task description cannot be empty.")
     return description.strip()
 
 
 def validate_due_date(due_date):
-    if not isinstance(due_date, str) or len(due_date.strip()) == 0:
+    if not isinstance(due_date, str):
+        raise ValueError("Due date must be a string.")
+    if len(due_date.strip()) == 0:
         raise ValueError("Due date cannot be empty.")
 
     try:
